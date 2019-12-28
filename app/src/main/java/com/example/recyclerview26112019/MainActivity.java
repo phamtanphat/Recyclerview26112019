40fcb48a86bc976c31acb03281715f679ddec42d
phatdroid94 <phatdroid94@gmail.com>
2019-12-28 19:31:40 +0700

    part 1

--- app/src/main/java/com/example/recyclerview26112019/MainActivity.java
+++ app/src/main/java/com/example/recyclerview26112019/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.recyclerview26112019;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView mRcvMonan;
+    MonanAdapter mMonanAdapter;
+    ArrayList<Monan> mArraylistMonan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
